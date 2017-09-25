bdabeac97b91ec6498d04117b30f127adefbd68d
Michael Kerr <mkerr09@gmail.com>
2017-09-24 20:54:42 -0400

    Add average taked 2

--- calc_metrics.py
+++ calc_metrics.py
@@ -17,12 +17,14 @@ if __name__ == '__main__':
     takt_distro = {}
     issue_list = []
     duration_list = []
+    average_takt_list = []
 
     for epic in processed_data:
         ##################################################
         # Get the duration of the epic
         ##################################################
         duration_list.append(sum(epic))
+        average_takt_list.append(float(sum(epic))/float(len(epic)))
 
         ##################################################
         # Update the distro of "takt" times
@@ -40,8 +42,9 @@ if __name__ == '__main__':
     metric_dict = {}
 
     #Average (days/item)
-    metric_dict['epic_average'] = np.average(duration_list)
-    metric_dict['takt_average'] = np.average(issue_list)
+    metric_dict['epic_avg'] = np.average(duration_list)
+    metric_dict['takt_avg'] = np.average(issue_list)
+    metric_dict['takt_avg2'] = np.average(average_takt_list)
 
     # Median
     metric_dict['epic_median'] = np.median(duration_list)
